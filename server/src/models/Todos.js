77d6a5cc456aba2db5b698effe77ccc5519040be
andnil5 <andnil5@kth.se>
2021-04-21 17:16:26 +0200

    fix: resolve linting issues

--- server/src/models/Todos.js
+++ server/src/models/Todos.js
@@ -12,41 +12,40 @@
  * @property {number} id - A unique identifier.
  * @property {string} name - The name of the todo.
  */
- const todos = {};
- let nextID = 0;
- 
- module.exports = {
-   /**
+const todos = {};
+let nextID = 0;
+
+module.exports = {
+  /**
    * Create a new todo.
    *
    * @param {string} name - The name of the new todo.
    * @return {number} The ID of the newly created todo.
    */
-   create(name) {
-     todos[nextID] = {
-       id: nextID,
-       name,
-     };
-     nextID += 1;
-     return todos[nextID - 1];
-   },
-   /**
+  create(name) {
+    todos[nextID] = {
+      id: nextID,
+      name,
+    };
+    nextID += 1;
+    return todos[nextID - 1];
+  },
+  /**
    * Delete a todo from the Todo model.
    *
    * @param {number} id - The ID of the todo to delete.
    * @return {boolean} Whether the deletion was successfull.
    */
-   delete(id) {
-     return todos[id] ? delete todos[id] : false;
-   },
-   /**
+  delete(id) {
+    return todos[id] ? delete todos[id] : false;
+  },
+  /**
    * Get a Specific Todo by id.
    *
    * @param {number} id - The ID of the todo.
    * @return {Todo} The requested todo.
    */
-   get(id) {
-     return todos[id];
-   },
- };
- 
+  get(id) {
+    return todos[id];
+  },
+};
